b9578921cc6cf3e8f5c121f64aeb1359c7c2c62f
Konstantin Vyunov <konstantin.s.vyunov@gmail.com>
2022-11-19 19:00:41 +0400

    Home task. Initial commit.

---
+++ Basic_Programming_C++/OOP_I/Task_1_Addresses/Task_1_Addresses.cpp
@@ -0,0 +1,71 @@
+// Program takes addresses from the "in.txt" file, reorders them upside down, and saves into the "out.txt" file.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Address {
+private:
+	std::string city;
+	std::string street;
+	int building = 0;
+	int appartment = 0;
+public:
+	Address() {}
+	Address(std::string city, std::string street, int building, int appartment) {
+		this->city = city;
+		this->street = street;
+		this->building = building;
+		this->appartment = appartment;
+	}
+
+	std::string set_stringed_address() {
+		return this->city + ' ' + this->street + ' ' + std::to_string(this->building) + ' ' + std::to_string(this->appartment);
+	}
+};
+
+std::string* readFile(int& address_quantity) {
+	std::string* addresses = nullptr;
+	std::ifstream file("in.txt");
+	if (!file.is_open()) {
+		std::cout << "Cannot open the file \"in.txt\"!" << std::endl;
+	} else {
+		file >> address_quantity;
+		int limit = 4 * address_quantity;
+		addresses = new std::string[limit];
+		for (int i = 0; i < limit; ++i) {
+			file >> addresses[i];
+		}
+		file.close();
+	}
+	return addresses;
+}
+
+Address* createList(std::string* raw_addresses, int address_quantity) {
+	int limit = 4 * address_quantity;
+	Address* list = new Address[address_quantity];
+	for (int i = 0, j = 0; i < address_quantity && j < limit; ++i, j += 4) {
+		list[i] = {raw_addresses[j+0], raw_addresses[j+1], stoi(raw_addresses[j+2]), stoi(raw_addresses[j+3])};
+	}
+	return list;
+}
+
+void writeFile(Address* list, const int address_quantity) {
+	std::ofstream file("out.txt");
+	file << address_quantity << std::endl;
+	for (int i = address_quantity - 1; i >= 0; --i) {
+		file << list[i].set_stringed_address() << std::endl;
+	}
+	file.close();
+	return;
+}
+
+int main(int argc, char** argv) {
+	int address_quantity = 0;
+	std::string* raw_addresses = readFile(address_quantity);
+	Address* list = createList(raw_addresses, address_quantity);
+	writeFile(list, address_quantity);
+	delete[] raw_addresses;
+	delete[] list;
+	return 0;
+}
