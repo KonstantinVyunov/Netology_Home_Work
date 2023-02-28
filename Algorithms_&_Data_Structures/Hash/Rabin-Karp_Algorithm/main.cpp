fbe972fc14f1cf64a4e88bb19b2a2dcb1fa7ecec
Konstantin Vyunov <konstantin.s.vyunov@gmail.com>
2023-03-01 01:45:43 +0400

    3rd task simplified

--- Algorithms_&_Data_Structures/Hash/Rabin-Karp_Algorithm/main.cpp
+++ Algorithms_&_Data_Structures/Hash/Rabin-Karp_Algorithm/main.cpp
@@ -2,13 +2,11 @@
 #include <string>
 #include <cmath>
 
-uint64_t stringHash(const std::string& word) {
-	const int p = 137;
+uint64_t simpleStringHash(const std::string& word) {
 	uint64_t hash = 0;
 	if (!word.empty()) {
-		size_t range = word.size();
-		for (size_t i = 0; i < range; ++i) {
-			hash += (static_cast<uint64_t>(pow(p, i)) * word[i]);
+		for (const char& chr : word) {
+			hash += chr;
 		}
 	} else {
 		std::cout << "Empty input!";
@@ -16,34 +14,24 @@ uint64_t stringHash(const std::string& word) {
 	return hash;
 }
 
-size_t findSubstringLightRabinKarp(const std::string& line, const std::string& word) {
-	size_t index = 0;
-	int module = 1'000;
+int64_t simpleRabinKarpToFindSubstring(const std::string& line, const std::string& word) {
+	int64_t index = 0;
 
-	const uint64_t word_hash = (stringHash(word) % module);
-	std::cout << "Word \"" << word << "\" hash: " << word_hash << std::endl; // <---
+	const uint64_t word_hash = simpleStringHash(word);
 	uint64_t subline_hash = 0;
 
 	const size_t word_range = word.size();
-	size_t line_range = line.size();
+	const size_t line_range = line.size();
 	
 	for (index = 0; index < (line_range - word_range + 1); ++index) {
 		if (index == 0) {
-			std::string subline;
 			for (size_t i = index; i < word_range; ++i) {
-				subline += line[i];
+				subline_hash += line[i];
 			}
-			subline_hash = (stringHash(subline) % module);
-			std::cout << line[index] << " Subline (" << subline << ") hash: " << subline_hash << std::endl; // <---
 		} else {
-			char chr_sub = line[index - 1];
-			char chr_add = line[index + word_range - 1];
-
 			char sub_chr = line[index - 1];
 			char add_chr = line[index + word_range - 1];
-			subline_hash = (subline_hash - sub_chr + add_chr);	// <---- HELP NEEDED!
-
-			std::cout << line[index] << " Subline (sub " << chr_sub << ", add " << chr_add << ") hash: " << subline_hash % module << std::endl; // <---
+			subline_hash = (add_chr + subline_hash - sub_chr);
 		}
 		if (word_hash != subline_hash) {
 			continue;
@@ -56,7 +44,7 @@ size_t findSubstringLightRabinKarp(const std::string& line, const std::string& w
 			return index;
 		}
 	}
-	return index;
+	return -1;
 }
 
 int main(int argc, char** argv) {
@@ -70,7 +58,13 @@ int main(int argc, char** argv) {
 	do {
 		std::cout << "Enter the word to search: ";
 		std::cin >> word;
-		std::cout << findSubstringLightRabinKarp(line, word) << std::endl;
+		if (simpleRabinKarpToFindSubstring(line, word) >= 0) {
+			std::cout << "The word \"" << word << "\" found at index: ";
+			std::cout << simpleRabinKarpToFindSubstring(line, word) << std::endl;
+		} else {
+			std::cout << "The word \"" << word << "\" is not found." << std::endl;
+		}
+
 	} while (word != "exit");
 
 	return EXIT_SUCCESS;
