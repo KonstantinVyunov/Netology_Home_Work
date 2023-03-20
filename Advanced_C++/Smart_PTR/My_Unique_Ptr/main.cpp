31825388f9be440e6e1b9e62f0e78ca8265ce0a4
Konstantin Vyunov <konstantin.s.vyunov@gmail.com>
2023-03-20 18:14:33 +0300

    second try

--- Advanced_C++/Smart_PTR/My_Unique_Ptr/main.cpp
+++ Advanced_C++/Smart_PTR/My_Unique_Ptr/main.cpp
@@ -1,54 +1,39 @@
 #include <iostream>
-#include <memory>
 
 template<class T>
 class MyUniquePtr {
-public:
-	T* ptr_ = nullptr;
+private:
+	T* smart_ptr = nullptr;
 	
 public:
-	MyUniquePtr(T* ptr) : ptr_(ptr) {
-		T* temp = nullptr;
-
-	}
+	MyUniquePtr(T* ptr) : smart_ptr(std::move(ptr)) {}
 
 	MyUniquePtr(const MyUniquePtr& other) = delete;
 	MyUniquePtr& operator=(const MyUniquePtr& other) = delete;
 	
-	T& operator*() { return *ptr_; }
-	const T& operator*() const { return *ptr_; }
+	T& operator*() { return *smart_ptr; }
+	const T& operator*() const { return *smart_ptr; }
 
-	const T& operator[](const int& index) const { return ptr_[index]; }
-	T& operator[](const int& index) { return ptr_[index]; }
-
-	T* release() {
-		T* temp_ptr = ptr_;
-		delete ptr_;
-		return temp_ptr;
+	T* release() noexcept {
+		T* temp_ptr = nullptr;
+		return std::exchange(smart_ptr, temp_ptr);
 	}
 
 	~MyUniquePtr() {
 		std::cout << "memory released" << std::endl;
-		delete[] ptr_;
+		delete smart_ptr;
 	}
 }; 
 
 int main(int argc, char** argv) {
-	int* i_ptr = new int[3] {1, 2, 3};
-	std::cout << "Original address: " << i_ptr << std::endl;
+	int* i_ptr = new int(3);
+	std::cout << "Raw pointer: " << i_ptr << std::endl;
 
 	MyUniquePtr<int> m(i_ptr);
-	std::cout << "  Passed address: " << &m << std::endl;
-	std::cout << "  Index value: " << m[2] << std::endl;
-	
-	delete[] i_ptr;
+	std::cout << "Get value: " << *m << std::endl;
+	std::cout << "Release ptr: " << m.release() << std::endl;
+
+	delete i_ptr;
 
 	return EXIT_SUCCESS;
 }
-
-
-//Конструктор, принимающий сырой указатель. - done!
-//Перегружен оператор* для получения объекта.
-//Запрещены оператор присваивания и конструктор копирования. - done!
-//Метод release, который освобождает владение и возвращает сырой указатель.
-//Деструктор.
