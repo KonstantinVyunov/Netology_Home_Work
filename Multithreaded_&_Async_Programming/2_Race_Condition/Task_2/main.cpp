4e9a890aebec138c65b3d682923fa41bc1081de6
Konstantin Vyunov <konstantin.s.vyunov@gmail.com>
2024-01-25 21:54:08 +0300

    to merge

--- Multithreaded_&_Async_Programming/2_Race_Condition/Task_2/main.cpp
+++ Multithreaded_&_Async_Programming/2_Race_Condition/Task_2/main.cpp
@@ -5,50 +5,52 @@
 #include <mutex>
 
 #include <Windows.h>
-#include "Define.h"
 
-std::mutex m;
+std::mutex mut;
 
 int main() {
-	auto handle = GetStdHandle(STD_OUTPUT_HANDLE);//избавляемся от курсора
+	auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO structCursorInfo;
 	GetConsoleCursorInfo(handle, &structCursorInfo);
 	structCursorInfo.bVisible = FALSE;
 	SetConsoleCursorInfo(handle, &structCursorInfo);
 
-	setlocale(0, "");
-	srand(time(0));//вместо длинны расчета
-	std::vector<std::thread> th;
-	const int kolvo_potokov = 20;
-	COORD mass[kolvo_potokov];
-	auto func = [&](int potok, int dlinna) {
+	srand(time(NULL));
+	std::vector<std::thread> vect;
+	const int thread_num = 20;
+	COORD mass[thread_num]{};
+	auto function = [&](int thread, int size) {
 		auto start = std::chrono::steady_clock::now();
-		mass[potok].X = 0;
-		mass[potok].Y = potok;
-		m.lock();
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mass[potok]);
-		std::cout << "номер потока: "; Y1 std::cout << potok; W1 std::cout << "  ID потока: "; Y1 std::cout << std::this_thread::get_id() << "\t "; W1
-			m.unlock();
+		mass[thread].X = 0;
+		mass[thread].Y = thread;
+		mut.lock();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mass[thread]);
+		std::cout << "thread #";
+		std::cout << thread;
+		std::cout << "\tthread ID: ";
+		std::cout << std::this_thread::get_id() << " \t ";
+		mut.unlock();
 		CONSOLE_SCREEN_BUFFER_INFO x;
 		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &x);
-		mass[potok].X = x.dwCursorPosition.X;
-		for (int i = 0; i < dlinna; ++i) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(dlinna * 100));
-			++mass[potok].X;
-			std::lock_guard<std::mutex> g(m);
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mass[potok]);
-			G1 std::cout << "="; W1
+		mass[thread].X = x.dwCursorPosition.X;
+		for (int i = 0; i < size; ++i) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(size * 100));
+			++mass[thread].X;
+			std::lock_guard<std::mutex> g(mut);
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mass[thread]);
+			std::cout << "=";
 		}
 		auto end = std::chrono::steady_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - start;
-		std::lock_guard<std::mutex> g(m);
-		std::cout << " Время расчета: " << " "; R1 std::cout << elapsed_seconds.count() << "s\t"; W1
+		std::lock_guard<std::mutex> g(mut);
+		std::cout << " time:" << " ";
+		std::cout << elapsed_seconds.count() << "s\t";
 		};
-	for (int i = 0; i < kolvo_potokov; ++i)
-		th.emplace_back(func, i, 5 + rand() % 10);
-	for (auto& t : th)
-		t.join();
-	COORD coord{ 0,kolvo_potokov };
+	for (int i = 0; i < thread_num; ++i)
+		vect.emplace_back(function, i, 5 + rand() % 10);
+	for (auto& elm : vect)
+		elm.join();
+	COORD coord{0,thread_num};
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
 	return EXIT_SUCCESS;
--- Multithreaded_&_Async_Programming/2_Race_Condition/Task_3/main.cpp
+++ Multithreaded_&_Async_Programming/2_Race_Condition/Task_3/main.cpp
@@ -4,45 +4,45 @@
 
 class Data {
 public:
-	char q;
-	int w;
-	double e;
+	char ch;
+	int i;
+	double d;
 	std::mutex m;
-	Data (char q, int w, double e) : q{ q }, w{ w }, e{ e } {}
+	Data (char q, int w, double e) : ch{ch}, i{i}, d{d} {}
 	void print() {
-		std::cout << "Q=" << q << " W=" << w << " E=" << e << "\n";
+		std::cout << "Ch = " << ch << " I = " << i << " D = " << d << "\n";
 	}
 };
 
-void swap_1(Data& obj1, Data& obj2) {
-	char q1; int w1; double e1;
+static void swap_1(Data& obj1, Data& obj2) {
+	char ch_1{}; int i_1{ 0 }; double d_1{0.0};
 	std::lock(obj1.m, obj2.m);
-	q1 = obj1.q; obj1.q = obj2.q; obj2.q = q1;
-	w1 = obj1.w; obj1.w = obj2.w; obj2.w = w1;
-	e1 = obj1.e; obj1.e = obj2.e; obj2.e = e1;
+	ch_1 = obj1.ch; obj1.ch = obj2.ch; obj2.ch = ch_1;
+	i_1 = obj1.i; obj1.i = obj2.i; obj2.i = i_1;
+	d_1 = obj1.d; obj1.d = obj2.d; obj2.d = d_1;
 	obj1.m.unlock();
 	obj2.m.unlock();
 }
 
-void swap_2(Data& obj1, Data& obj2) {
-	char q1; int w1; double e1;
+static void swap_2(Data& obj1, Data& obj2) {
+	char ch_1{}; int i_1{ 0 }; double d_1{0.0};
 	std::unique_lock<std::mutex> u1(obj1.m, std::defer_lock);
 	std::unique_lock<std::mutex> u2(obj2.m, std::defer_lock);
 	std::lock(u1, u2);
-	q1 = obj1.q; obj1.q = obj2.q; obj2.q = q1;
-	w1 = obj1.w; obj1.w = obj2.w; obj2.w = w1;
-	e1 = obj1.e; obj1.e = obj2.e; obj2.e = e1;
+	ch_1 = obj1.ch; obj1.ch = obj2.ch; obj2.ch = ch_1;
+	i_1 = obj1.i; obj1.i = obj2.i; obj2.i = i_1;
+	d_1 = obj1.d; obj1.d = obj2.d; obj2.d = d_1;
 }
 
-void swap_3(Data& obj1, Data& obj2) {
-	char q1; int w1; double e1;
+static void swap_3(Data& obj1, Data& obj2) {
+	char ch_1; int i_1; double d_1;
 	std::scoped_lock<std::mutex,std::mutex> u1(obj1.m, obj2.m);
-	q1 = obj1.q; obj1.q = obj2.q; obj2.q = q1;
-	w1 = obj1.w; obj1.w = obj2.w; obj2.w = w1;
-	e1 = obj1.e; obj1.e = obj2.e; obj2.e = e1;
+	ch_1 = obj1.ch; obj1.ch = obj2.ch; obj2.ch = ch_1;
+	i_1 = obj1.i; obj1.i = obj2.i; obj2.i = i_1;
+	d_1 = obj1.d; obj1.d = obj2.d; obj2.d = d_1;
 }
 
-void func(void (*ptr_swap)(Data&, Data&), Data& obj1, Data& obj2) {
+static void func(void (*ptr_swap)(Data&, Data&), Data& obj1, Data& obj2) {
 	for (int i = 0; i < 1000001; ++i)
 		ptr_swap(obj1, obj2);
 }
@@ -69,7 +69,7 @@ int main() {
 	}
 
 	{
-		std::cout << "\n\n";
+		std::cout << "\n";
 		Data d1('1', 1, 1.0);
 		Data d2('2', 2, 2.0);
 		std::cout << "Начальные данные\n";
@@ -89,7 +89,7 @@ int main() {
 	}
 
 	{
-		std::cout << "\n\n";
+		std::cout << "\n";
 		Data d1('1', 1, 1.0);
 		Data d2('2', 2, 2.0);
 		std::cout << "Начальные данные\n";
@@ -107,4 +107,5 @@ int main() {
 		std::cout << "d1: "; d1.print();
 		std::cout << "d2: "; d2.print();
 	}
+	return EXIT_SUCCESS;
 }
