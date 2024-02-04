a4927672b6fb0da938ce510a87ada95e614a332c
Konstantin Vyunov <konstantin.s.vyunov@gmail.com>
2024-02-04 22:57:16 +0300

    update

--- Database/5.PostgreSQL.C++/Task1.cpp
+++ Database/5.PostgreSQL.C++/Task1.cpp
@@ -1,74 +1,100 @@
-﻿#include <iostream>
+﻿#include <pqxx/pqxx>
+#include <iostream>
 #include <iomanip>
 #include <exception>
-#include <pqxx/pqxx>
 
-class BD {
+class DataBase {
 private:
-	pqxx::connection& obj;
+	pqxx::connection& connect_obj;
 public:
-	BD(pqxx::connection& obj) :obj(obj) {}
-	BD() = delete;
-	BD(const BD& obj) = delete;
-	BD(BD&& obj) = delete;
-	BD& operator=(const BD& obj) = delete;
-	BD& operator=(BD&& obj) = delete;
+	DataBase(pqxx::connection& connection) : connect_obj(connection) {}
+	DataBase() = delete;
+	DataBase(const DataBase& connect_obj) = delete;
+	DataBase(DataBase&& connect_obj) = delete;
 
-	void CREATE_TABLE(std::string str) {
-		pqxx::work w{obj};
-		w.exec("create table if not exists " + w.esc(str));
-		w.commit();
-	}
+	DataBase& operator=(const DataBase& connect_obj) = delete;
+	DataBase& operator=(DataBase&& connect_obj) = delete;
 
-	void INSERT_FIO(std::string str, std::string str1 = "", std::string str2 = "", std::string str3 = "", std::string str4 = "") {
-		pqxx::work w{obj};
-		w.exec("insert into " + w.esc(str) + " values (" + w.esc(str1) + ", '" + w.esc(str2) + "', '" + w.esc(str3) + "', '" + w.esc(str4) + "') ");
-		w.commit();
-	}
-	void INSERT_T_FONE(std::string str, std::string str1 = "", std::string str2 = "", std::string str3 = "") {
-		pqxx::work w{obj};
-		w.exec("insert into " + w.esc(str) + " values (" + w.esc(str1) + ", " + w.esc(str2) + ", '" + w.esc(str3)+ "') ");
-		w.commit();
+	void CreateTable(std::string sql_query) {
+		pqxx::transaction transaction_create_table{ connect_obj };
+		transaction_create_table.exec("CREATE TABLE IF NOT EXISTS " + transaction_create_table.esc(sql_query) );
+
+		transaction_create_table.commit();
 	}
 
-	void UPDATE(std::string str1, std::string str2, std::string str3, std::string str4) {
-		pqxx::work w{obj};
-		w.exec("update " + w.esc(str1) +" set "+ w.esc(str2)+ "='" + w.esc(str3) + "' " + " where " + w.esc(str2)+"='"+ w.esc(str4)+"'");
-		w.commit();
+	void InsertName(std::string str, std::string str1 = "", std::string str2 = "", std::string str3 = "", std::string str4 = "") {
+		pqxx::transaction transaction_insert_name{ connect_obj };
+		transaction_insert_name.exec("insert into "+ transaction_insert_name.esc(str)
+									 + " values (" + transaction_insert_name.esc(str1) + ", '"
+												   + transaction_insert_name.esc(str2) + "', '"
+												   + transaction_insert_name.esc(str3) + "', '"
+												   + transaction_insert_name.esc(str4) + "') ");
+
+		transaction_insert_name.commit();
 	}
-	void DELETE_STR_T_FONE( std::string str1, std::string str2) {
-		pqxx::work w{obj};
-		w.exec("delete from T_FONE  where " + w.esc(str1) + " = " + w.esc(str2));
-		w.commit();
+	void InsertPhoneNumber(std::string str, std::string str1 = "", std::string str2 = "", std::string str3 = "") {
+		pqxx::transaction transaction_insert_phone{ connect_obj };
+		transaction_insert_phone.exec("insert into "+ transaction_insert_phone.esc(str)
+									  + " values (" + transaction_insert_phone.esc(str1) + ", "
+													+ transaction_insert_phone.esc(str2) + ", '"
+													+ transaction_insert_phone.esc(str3) + "') ");
+
+		transaction_insert_phone.commit();
 	}
-	void DELETE_STR_FIO(std::string str1, std::string str2) {
-		DELETE_STR_T_FONE( str1 + "_fio", str2);
-		pqxx::work w{obj};
-		w.exec("delete from FIO where " + w.esc(str1) + " = " + w.esc(str2));
-		w.commit();
+
+	void Update(std::string str1, std::string str2, std::string str3, std::string str4) {
+		pqxx::transaction transaction_update{ connect_obj };
+		transaction_update.exec("update " + transaction_update.esc(str1)
+								+ " set " + transaction_update.esc(str2)
+								+ "='" + transaction_update.esc(str3)
+								+ "' " + " where "
+												 + transaction_update.esc(str2) + "='"
+												 + transaction_update.esc(str4) + "'");
+
+		transaction_update.commit();
 	}
-	void POISK(std::string str) {
-		pqxx::work w{obj};
+	void DeletePhone( std::string str1, std::string str2) {
+		pqxx::transaction transaction_delete_phone{ connect_obj };
+		transaction_delete_phone.exec("delete from T_FONE  where "
+										+ transaction_delete_phone.esc(str1)
+										+ " = " + transaction_delete_phone.esc(str2));
+		
+		transaction_delete_phone.commit();
+	}
+	void DeleteName(std::string str1, std::string str2) {
+		DeletePhone( str1 + "_fio", str2);
+		pqxx::transaction transaction_delete_name{ connect_obj };
+		transaction_delete_name.exec("delete from FIO where "
+										+ transaction_delete_name.esc(str1)
+										+ " = " + transaction_delete_name.esc(str2));
+		
+		transaction_delete_name.commit();
+	}
+	void Search(std::string str) {
+		pqxx::transaction transaction_search{ connect_obj };
 		std::cout << "id    Имя      Фамилия   почта   id  телефон  \n";
-		for (auto [id, first_name, last_name, e_mail, t_id, tel1] : w.query<int, std::string, std::string, std::string, int, std::string >(
-			"SELECT f.id, f.first_name, f.last_name,f.e_mail, t.id, t.t_fone FROM FIO as f "
-			"join T_FONE as t on f.id = t.id_fio "
-		    "where (f.first_name = '" + w.esc(str) + "' "+
-			"or f.last_name = '" + w.esc(str) + "' "+
-			"or f.e_mail = '" + w.esc(str) + "' "+
-			"or t.t_fone = '" + w.esc(str) + "' )"))
+		for (auto [id, first_name, last_name, e_mail, t_id, tel1] : transaction_search.query<int, std::string, std::string, std::string, int, std::string>
+				(
+				"SELECT f.id, f.first_name, f.last_name,f.e_mail, t.id, t.t_fone FROM FIO as f "
+				"join T_FONE as t on f.id = t.id_fio "
+				"where (f.first_name = '" + transaction_search.esc(str) + "' "+
+				"or f.last_name = '" + transaction_search.esc(str) + "' "+
+				"or f.e_mail = '" + transaction_search.esc(str) + "' "+
+				"or t.t_fone = '" + transaction_search.esc(str) + "' )"
+				)
+			)
 		{
 			std::cout << id << std::setw(10) << first_name << std::setw(10) << last_name << std::setw(10) << e_mail << std::setw(3) << t_id << std::setw(8) << tel1 << "\n";
 		}
 		std::cout << "\n";
 	}
 	void DELETE_TABLE(std::string str) {
-		pqxx::work w{obj};
+		pqxx::transaction w{ connect_obj };
 		w.exec("drop table " + w.esc(str));
 		w.commit();
 	}
 	void PRINT_BD() {
-		pqxx::work w{obj};
+		pqxx::transaction w{ connect_obj };
 		std::cout << "id    Имя      Фамилия   почта   id  телефон  \n";
 		for (auto [id, first_name, last_name, e_mail, t_id, tel1] : w.query<int, std::string, std::string, std::string, int, std::string >(
 			"SELECT f.id, f.first_name, f.last_name,f.e_mail, t.id, t.t_fone FROM FIO as f "
@@ -84,59 +110,60 @@ public:
 int main()
 {
 	setlocale(0, "");
+
 	try	{
-		pqxx::connection c("host=localhost port=5432 dbname=TEST user=postgres password=12345678");
-		BD bd(c);
-		bd.CREATE_TABLE("FIO(id int PRIMARY KEY not null,"
+		pqxx::connection connect_obj("host=localhost port=5432 dbname=test user=postgres password=finik");
+		DataBase data_base(connect_obj);
+		data_base.CreateTable("FIO(id int PRIMARY KEY not null,"
 			                "first_name varchar(30) not null,"
 			                "last_name varchar(30) not null,"
 			                "e_mail varchar(50))");		
-		bd.CREATE_TABLE("T_FONE(id int PRIMARY KEY not null,"
+		data_base.CreateTable("T_FONE(id int PRIMARY KEY not null,"
 			                   "id_fio int references FIO (id),"
 			                   "t_fone varchar(20))");
-		bd.INSERT_FIO("FIO(id, first_name, last_name,e_mail )", "1", "Andrey", "Ivanov", "ai@m.ru");
-		bd.INSERT_FIO("FIO(id, first_name, last_name,e_mail)", "2", "Sergey", "Petrov","sp@g.com");
-		bd.INSERT_FIO("FIO(id, first_name, last_name,e_mail)",  "3", "Vasiliy", "Sidorov","vs@y.ru");
-		bd.INSERT_FIO("FIO(id, first_name, last_name,e_mail)",  "4", "Dmitriy", "Rogov","dr@r.ru");
+		data_base.InsertName("FIO(id, first_name, last_name,e_mail )", "1", "Andrey", "Ivanov", "ai@m.ru");
+		data_base.InsertName("FIO(id, first_name, last_name,e_mail)", "2", "Sergey", "Petrov","sp@g.com");
+		data_base.InsertName("FIO(id, first_name, last_name,e_mail)",  "3", "Vasiliy", "Sidorov","vs@y.ru");
+		data_base.InsertName("FIO(id, first_name, last_name,e_mail)",  "4", "Dmitriy", "Rogov","dr@r.ru");
 
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "1", "1", "+7991");
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "2", "1", "");
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "3", "2", "+7992");
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "4", "3", "+7993");
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "5", "3", "+7994");
-		bd.INSERT_T_FONE("T_FONE(id, id_fio, t_fone)",  "6", "4", "+7998");	
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "1", "1", "+7991");
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "2", "1", "");
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "3", "2", "+7992");
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "4", "3", "+7993");
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "5", "3", "+7994");
+		data_base.InsertPhoneNumber("T_FONE(id, id_fio, t_fone)",  "6", "4", "+7998");
 		std::cout << "Начальный вид базы\n";
-		bd.PRINT_BD();
+		data_base.PRINT_BD();
 		
-		bd.UPDATE("FIO", "first_name","Griha", "Andrey");	
+		data_base.Update("FIO", "first_name","Griha", "Andrey");
 		std::cout << "UPDATE 'Andrey' на 'Griha'\n";
-		bd.PRINT_BD();
+		data_base.PRINT_BD();
 		
-		bd.DELETE_STR_T_FONE("id"," 1 ");
+		data_base.DeletePhone("id"," 1 ");
 		std::cout << "DELETE_STR_T_FONE удаление строки с телефоном 'id=1'\n";
-		bd.PRINT_BD();
+		data_base.PRINT_BD();
 		
-		bd.DELETE_STR_FIO( "id", "1");
+		data_base.DeleteName( "id", "1");
 		std::cout << "DELETE_STR_FIO удаление строки с клиентом 'id=1'\n";
-		bd.PRINT_BD();
+		data_base.PRINT_BD();
 
 		std::cout << "Поиск по имени 'Sergey'\n";
-		bd.POISK("Sergey");
+		data_base.Search("Sergey");
 		std::cout << "Поиск по фамилии 'Sidorov'\n";
-		bd.POISK("Sidorov");
+		data_base.Search("Sidorov");
 		std::cout << "Поиск по почте 'dr@r.ru'\n";
-		bd.POISK("dr@r.ru");
+		data_base.Search("dr@r.ru");
 		std::cout << "Поиск по номеру телефона '+7993'\n";
-		bd.POISK("+7993");	
+		data_base.Search("+7993");
 
-		bd.DELETE_TABLE("T_FONE");
-		bd.DELETE_TABLE("FIO");
+		data_base.DELETE_TABLE("T_FONE");
+		data_base.DELETE_TABLE("FIO");
 
 
 	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
+	catch (std::exception& error) {
+		std::cerr << error.what() << std::endl;
 	}
+
 	return EXIT_SUCCESS;
 }
